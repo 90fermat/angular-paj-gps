7132d555b05e609ee10d3a9e505d2acfae763561
90fermat <90fermat@gmail.com>
2023-04-08 19:32:07 +0100

    - add route for a device

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginService } from '../services/login.service';
 import { Device } from '../models/device';
-import { HttpClient } from '@angular/common/http';
+import {HttpClient, HttpParams} from '@angular/common/http';
 import { Map, LngLatLike } from 'maplibre-gl';
 import { map } from 'rxjs/operators';
 import { firstValueFrom } from 'rxjs';
+import {CustomHttpParamEncoder} from "../helpers/custom-encoder";
 
 @Component({
   selector: 'app-home',
@@ -19,6 +20,8 @@ export class HomeComponent implements OnInit {
   center: LngLatLike = { lng: 7.11, lat: 50.11 }
   devices: Device[]
   devicesLastPositions: DevicePos[]
+  devicesLastPoints: any = []
+  geometry: any
   
   private baseUrl: String = 'https://connect.paj-gps.de/api/';
 
@@ -27,9 +30,6 @@ export class HomeComponent implements OnInit {
     private http: HttpClient)  {
     const user = { email: 'testkunde@paj-gps.de', password: 'app123#.'}
     loginService.login(user);
-
-    //this.getAllLastPositions(this.devices.then(value => value.map(device => device.id)))
-
   }
 
   async ngOnInit() {
@@ -37,7 +37,10 @@ export class HomeComponent implements OnInit {
     this.devicesLastPositions = await this.getAllLastPositions((this.devices).map(device => device.id))
     console.log("Devices:", this.devices)
     console.log("DevicesPos:", this.devicesLastPositions)
-
+    for (const id of this.devices.map(value => value.id)) {
+      this.devicesLastPoints.push({id: id, last_points: await this.getDeviceLastPoints(id)});
+    }
+    console.log("DevicesPoints:", this.devicesLastPoints)
   }
 
   async getAllDevices() {
@@ -64,10 +67,34 @@ export class HomeComponent implements OnInit {
   }
 
   centerMapTo(device: Device) {
+    this._clearGeometry()
     this.center = this._devicePosToLngLatLike(
         this.devicesLastPositions.find(dev => device.id === dev.iddevice)
     )
-    this.zoom = 14;
+    this.zoom = 14
+    this._showLastPoints(device.id)
+  }
+
+  showLastPoints(deviceID: number) {
+    this._clearGeometry()
+    alert(this.searchDeviceName(deviceID))
+    setTimeout(() => this.geometry = {
+      type: 'LineString',
+      coordinates: this.devicesLastPoints.find(value => value.id === deviceID).last_points
+    }, 1000)
+
+    console.log("geometry:", this.geometry.coordinates)
+  }
+  _showLastPoints(deviceID: number) {
+    this._clearGeometry()
+    setTimeout(() => this.geometry = {
+      type: 'LineString',
+      coordinates: this.devicesLastPoints.find(value => value.id === deviceID).last_points
+    }, 1000)
+  }
+
+  _clearGeometry() {
+    this.geometry = undefined
   }
 
   _devicePosToLngLatLike(devicePos: DevicePos): LngLatLike {
@@ -76,6 +103,14 @@ export class HomeComponent implements OnInit {
     } else  {
       alert("The Device don't has a Last Tracked Position")
     }
+  }
+
+  async getDeviceLastPoints(deviceID: number) {
+    return firstValueFrom(this.http.get<any>(`${this.baseUrl}trackerdata/${deviceID}/last_points`,
+        { params: new HttpParams({ encoder: new CustomHttpParamEncoder()})
+              .append("lastPoints", 200)})
+        .pipe(map(response =>  (response.success as DevicePos[])
+            .map(devicePos => [devicePos.lng, devicePos.lat]))))
   }
 
   alert(message) {
