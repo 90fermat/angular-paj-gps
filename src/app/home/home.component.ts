ba5577a3cbbfe2475185e85057c8c4a3dcc29354
90fermat <90fermat@gmail.com>
2023-04-08 11:36:35 +0100

    fix map and marked devices

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Device } from '../models/device';
 import { HttpClient } from '@angular/common/http';
 import { Feature } from 'maplibre-gl';
 import { map } from 'rxjs/operators';
-import { async } from 'rxjs';
+import { async, interval, firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -14,69 +14,72 @@ import { async } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
   
-  public devices: Device[];
-  public devicesLastPositions: DevicePos[] | any
+  public devices: Device[]
+  public devicesLastPositions: DevicePos[]
   
   private baseUrl: String = 'https://connect.paj-gps.de/api/';
 
   constructor(
     public loginService: LoginService, 
-    private http: HttpClient) { 
-    const user = { email: 'testkunde@paj-gps.de', password: 'app123#.'};
+    private http: HttpClient)  {
+    const user = { email: 'testkunde@paj-gps.de', password: 'app123#.'}
     loginService.login(user);
-    this.getAllDevices();
+
+    //this.getAllLastPositions(this.devices.then(value => value.map(device => device.id)))
+
   }
 
-  ngOnInit() {
-    this.getAllLastPositions;
+  async ngOnInit() {
+    this.devices = await this.getAllDevices();
+    this.devicesLastPositions = await this.getAllLastPositions((this.devices).map(device => device.id))
+    console.log("Devices:", this.devices)
+    console.log("DevicesPos:", this.devicesLastPositions)
+
   }
 
-  getAllDevices() {
+  async getAllDevices() {
     if(this.loginService.isLogged) {
-      return this.http.get<any>(this.baseUrl + 'device')
-    .subscribe({
-      next(response){
-        this.devices = response.success
-      },
-      error(error) {
-        console.log(error.error)
-      }
-    })
+    /*return this.http.get<any>(this.baseUrl + 'device')
+  .subscribe({
+    next(response){
+      this.devices = response.success
+      console.log("Devices", this.devices)
+    },
+    error(error) {
+      console.log(error.error)
+    }
+  })*/
+      return firstValueFrom(this.http.get<any>(this.baseUrl + 'device')
+          .pipe(map(response => response.success)))
     } else {
-      this.devices = []
+      return  Promise.all([]);
     }
   }
 
-  getAllLastPositions(devicesIDS: number[]) {
-    let deviceIDs = { devicesIDS:  devicesIDS }
-    this.http.post<any>(`${this.baseUrl}trackerdata/getalllastpositions`,
-    { devicesIDS:  devicesIDS })
-    .pipe(map(response => response.success))
-    .subscribe({
-      next(val) {
-        this.devicesLastPositions = val;
-      },
-      error(error) {
-        console.log(error.error)
-      }
-    })
+  async getAllLastPositions(deviceIDS: number[]) {
+    return firstValueFrom(this.http.post<any>(`${this.baseUrl}trackerdata/getalllastpositions`,
+        { "deviceIDs":  deviceIDS })
+        .pipe(map(response => response.success)))
   }
 
-  searchDeviceColor(id: string) {
-    return this.devices.find((device) => id === `${device.id}`).spurfarbe
+  searchDeviceColor(id: number) {
+    //return this.devices.then(value => value.find((device) => id === `${device.id}`).spurfarbe)
+    return this.devices.find((device) => id === device.id).spurfarbe
   }
 
-  searchDeviceName(id: string) {
-    return this.devices.find((device) => id === `${device.id}`).name
+  searchDeviceName(id: number) {
+    //return this.devices.then(value => value.find((device) => id === `${device.id}`).name)
+    return this.devices.find((device) => id === device.id).name
   }
 
   alert(message) {
-    alert(message);
+    alert(message)
   }
 }
 
 export interface DevicePos {
-    id: string,
-    lat: number,
-    lgn: number;
+  id: string,
+  lat: number,
+  lng: number,
+  iddevice: number
 }
